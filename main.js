dd060e935235e7cc17046dc4f3ba20cc3d42be42
kame-ru <101446595+kame-ru@users.noreply.github.com>
2022-03-22 10:57:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
 'use strict'
 {
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-const ul = document.querySelector('.container>ul');
-const slides = ul.children;
-let currentIndex = 0;
-function moveSlides(){
-  const slideWidth = slides[0].getBoundingClientRect().width;
-  ul.style.transform=`translateX(${-1 * slideWidth * currentIndex }px)`
-}
-
-function updateButton(){
-  prev.classList.remove(`hidden`);
-  next.classList.remove(`hidden`);
-  if(currentIndex === 0){
-    prev.classList.add(`hidden`);
-  }
-  if(currentIndex === slides.length - 1){
-    next.classList.add(`hidden`);
-  }
-}
-
-updateButton();
-next.addEventListener('click', ()=>{
-  currentIndex++;
-  updateButton();
-  moveSlides();
-});
-prev.addEventListener('click', ()=>{
-  currentIndex--;
-  updateButton();
-  moveSlides();
-});
+  const adv = document.querySelector('footer img')
+  adv.addEventListener('click',()=>{
+    adv.style.transform='translateY(-1500px) rotate(7200deg)';
+  })
 }
